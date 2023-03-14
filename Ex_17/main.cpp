954d2c272298854425f73f0b9d61cd87cbc8ab1e
Greg <gd.temp@pm.me>
2023-03-14 14:37:46 +0100

    Everything work fine

--- Ex_17/main.cpp
+++ Ex_17/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    //Write a program that calculate the sqaure roots of user input. The program has to stop when user input = 0. User input must not be negative values
+    double userInput = 0;
+    
+    do {
+    std::cout << "Enter a positive number: " << std::endl;
+    std::cin >> userInput;
+    if (userInput < 0) {
+        std::cout << "Error!" << std::endl;  
+        continue;     
+    } else if (userInput == 0) {
+        break;
+    }
+
+    std::cout << "The square root is: " <<sqrt(userInput) << std::endl;
+    } while (userInput != 0);
 }
